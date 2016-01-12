679e1e83258334c29e5aa7e8ac32c849453b8fe0
admande <adam.mandeville@gmail.com>
2016-01-12 16:18:20 -0500

    finished search functionality

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,4 +7,12 @@ class Piece < ActiveRecord::Base
   { scope: :composer, message: "There should only be one title and composer pair" }
   validates :composer, presence: true
   validates :user_id, presence: { message: " must be signed in to create a new piece" }
+
+  def self.title_search(query)
+    where("title ilike ?", "%#{query}%")
+  end
+
+  def self.composer_search(query)
+    where("composer ilike ?", "%#{query}%")
+  end
 end
