62ea25e74da5404a9f4b36380069e0dd32ceb4be
AlexBennett <alexanderbondbennett@gmail.com>
2016-01-12 13:51:30 -0500

    added find_or_create_by and used faker for user_ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-Piece.delete_all
-Review.delete_all
-User.delete_all
-
 10.times do
-  User.create(
+  User.find_or_create_by(
     email: Faker::Internet.email,
     username: Faker::Internet.user_name,
     password: Faker::Internet.password,
@@ -11,111 +7,111 @@ User.delete_all
   )
 end
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Symphony No. 5 in D Major",
   composer: "Shostakovich",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Symphony No. 3 in E Minor, Eroica",
   composer: "Beethoven",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Piano Concerto No. 2 in G Minor",
   composer: "Prokofiev",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Violin Concerto No. 1 in D Major",
   composer: "Prokofiev",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Gaspard de la Nuit",
   composer: "Ravel",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Cello Concerto in B Major",
   composer: "Dvorak",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Rite of Spring",
   composer: "Stravinsky",
-  user_id: 3
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Firebird Suite",
   composer: "Stravinsky",
-  user_id: 1
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Bolero",
   composer: "Ravel",
-  user_id: 1
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Symphony No. 3 in F Major",
   composer: "Brahms",
-  user_id: 5
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Rhapsody in Blue",
   composer: "Gershwin",
-  user_id: 4
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "String Quartet No. 24 in G Major",
   composer: "Mozart",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Peter Grimes",
   composer: "Britten",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "War Requiem",
   composer: "Britten",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Danzon No. 2",
   composer: "Marquez",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Hebrides Overture",
   composer: "Mendelssohn",
-  user_id: 2
+  user_id: Faker::Number.between(1,10)
 )
 
-Piece.create(
+Piece.find_or_create_by(
   title: "Adagio for Strings",
   composer: "Barber",
-  user_id: 7
+  user_id: Faker::Number.between(1,10)
 )
 
 100.times do
-  Review.create(
-    user_id: Faker::Number.between(1, 10),
+  Review.find_or_create_by(
+    user_id: Faker::Number.between(1,10),
     piece_id: Faker::Number.between(1, 17),
     rating: Faker::Number.between(1, 10),
     title: Faker::Hipster.sentence(3),
