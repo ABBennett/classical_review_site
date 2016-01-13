000d2def66fb6cf3e70836ce0fd6642e6a60696a
David Harms <davidharms@MacBook-Pro.local>
2016-01-13 16:07:05 -0500

    finished admin deleting user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authorize_member
+
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted"
+    redirect_to users_path
   end
 end
