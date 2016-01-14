a2889c3a5aca8b3b9c9ce026a49ae2c4f111ddbd
Edwin Chow <echow6@gmail.com>
2016-01-14 13:00:24 -0500

    should be ready to go

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,11 @@ class  VotesController < ApplicationController
   respond_to :html, :json, :js
 
   def create
-    @vote = Vote.find_or_create_by(user_id: current_user.id, review_id: vote_params[:review_id])
-    @vote.update(vote_params)
+    @vote = Vote.find_or_create_by(user: current_user, review_id: vote_params[:review_id])
     respond_to do |format|
-      if @vote.save
+      if @vote.update(vote_params)
         @review = Review.find(params[:vote][:review_id])
-        format.html { redirect_to piece_path(@review.piece_id) }
+        format.html { redirect_to piece_path(@review.piece) }
         format.json   {
             render json: {
               up: @vote.up,
