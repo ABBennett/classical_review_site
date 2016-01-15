91f80d959a1382bf247b89242c39301a270fcae4
David Harms <davidharms@MacBook-Pro.local>
2016-01-15 11:30:19 -0500

    user can see profile

---
+++ spec/features/user_sees_own_profile_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'user sees profile' do
+  let(:user) { FactoryGirl.create(:user) }
+  scenario 'user clicks name on topbar' do
+    visit root_path
+    sign_in_as(user)
+    save_and_open_page
+    click_link "Signed in as #{user.username}"
+    expect(page).to have_content(user.email)
+
+    end
+end
